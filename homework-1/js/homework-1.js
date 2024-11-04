5ec7e8d7f5b3809fa9acdb643c2d9aa6c1209130
bakzhan <erzhanbakzhan0@gmail.com>
2024-11-04 23:19:13 +0500

    task 2

--- homework-1/js/homework-1.js
+++ homework-1/js/homework-1.js
@@ -1,8 +1,11 @@
-// task 1
+/* // task 1
 let age = prompt("Жасыңызды енгізіңіз: ");
 let a = 65;
 var student1 = 17;
 var student2 = 23;
-alert((age > a) || ((student1<=age) && (age<=student2)));
+alert((age > a) || ((student1<=age) && (age<=student2))); */
 
 //task2 
+let a = 10; b = 20;
+let number = Number(prompt("Write a number: "));
+alert((a < number) && (number < b));
