5b3f4ac632be2d934fc2473dde081333c6919348
bakzhan <erzhanbakzhan0@gmail.com>
2024-11-04 23:33:00 +0500

    task3

--- homework-1/js/homework-1.js
+++ homework-1/js/homework-1.js
@@ -5,7 +5,12 @@ var student1 = 17;
 var student2 = 23;
 alert((age > a) || ((student1<=age) && (age<=student2))); */
 
-//task2 
+/* //task2 
 let a = 10; b = 20;
 let number = Number(prompt("Write a number: "));
-alert((a < number) && (number < b));
+alert((a < number) && (number < b)); */
+
+//task3
+let num1 = prompt("Write first number: ")
+let num2 = prompt("Write second number: ")
+alert((num1 < 0 && num2 >= 0) || (num2 < 0 && num1 >= 0))
